3b72046f9f78fe3fe99785356e15265355d7e688
zhangshuo <443494891@qq.com>
2019-03-31 16:54:41 +0800

    change

--- 课程练习/LS08/0802.js
+++ 课程练习/LS08/0802.js
@@ -19,8 +19,30 @@ console.log(x);
 
 //
 var f2 = function (){
-    var y = "局部";
+    //var y = "局部";
+    y = "全局";
     console.log(y);
 };
 f2();
 console.log(y);//函数内的变量前有var ,则此行报错：y not defined
+//没有var 则此行输出全局变量y值
+
+if(true){
+    (function (){ //IIFE start
+        var z = 2;
+    }()); //IIFE end
+}
+console.log(z);//z is not defined
+/*IIFE 立即执行函数表达式
+
+*/
+function fn(){
+    var i = 1;
+    return function(){
+        console.log(++i);
+    }
+}
+fn();
+var foo = fn();
+foo();//2
+foo();//3
