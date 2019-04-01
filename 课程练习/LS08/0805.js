16b83d8e384a5bb80cc98e45ee961e56ca807c71
zhangshuo <443494891@qq.com>
2019-04-01 10:57:20 +0800

    js对象

--- 课程练习/LS08/0805.js
+++ 课程练习/LS08/0805.js
@@ -1,4 +1,18 @@
 /**对象 instanceof 构造函数
  * 判断对象是否可以使用构造函数实例化得到
  * 判断在对象的原型链上能否找到构造函数的原型
+ * 
+ * 对象.__proto__ == 构造函数.prototype
  */
+console.log(Object instanceof Function);//true
+console.log(Object.__proto__ == Function.prototype);//true
+
+console.log(Object instanceof Object);//true
+
+function Person(username,age){
+    this.username = username;
+    this.age = age;
+}
+var p = new Person("lisi",20);
+console.log(p.__proto__ == Person.prototype);
+console.log(p.__proto__.__proto__);
