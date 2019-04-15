c99b68496ea8aa689c96eeb69cd0aa053024a70e
zhangshuo <443494891@qq.com>
2019-04-15 10:25:15 +0800

    add

--- 课程练习/LS09/04.js
+++ 课程练习/LS09/04.js
@@ -1,4 +1,36 @@
 var obj1 = {z:1};
 var obj2 = Object.create(obj1);
 
-console.log(obj2.__proto__);
+Object.prototype.sayHello = function(){
+    console.log('hello world');
+}
+//原型链的尽头
+console.log(obj2.__proto__.__proto__.__proto__);
+obj2.sayHello();
+function Person(username,age){
+    this.username = username;
+    this.age = age;
+    this.sayHello = function(){
+        console.log('hello world');
+    }
+}
+var person1 = new Person('zhangsan',1);
+var person2 = new Person('lisi',2);
+console.log(person1.__proto__ == Person.prototype);
+console.log(person1,person2);
+
+//节省内存空间
+Person.prototype.sayHello = function(){
+    console.log('hello world');
+}
+var person1 = new Person('zhangsan',1);
+var person2 = new Person('lisi',2);
+//
+function Person(username,age){
+    this.username = username;
+    this.age = age;
+}
+Person.prototype.sayHello = function(){
+    console.log('hello world');
+}
+var person1 = new Person('zhangsan',1);
