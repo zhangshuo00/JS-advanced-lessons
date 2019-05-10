167880efbf2845e812f3e58157265d754b209e70
zhangshuo <443494891@qq.com>
2019-05-10 11:16:02 +0800

    正则表达式

--- 课程练习/LS12/01.js
+++ 课程练习/LS12/01.js
@@ -25,6 +25,8 @@ str1.replace(reg1,'x');
  *  \W: 不是字母数字下划线
  * 
  *  \s: 匹配空格，可以使space，tab
+ *  . : 匹配除换行符之外的任何单个字符
+ *  
  */
 var reg1 = /a\d/gi;
 var str1 = 'adfsjo1joi11xxxxoij2311';
@@ -43,7 +45,23 @@ var str4 = "a1 s2joi3";
 str4.replace(reg4,'x');
 
 
-/**字符的集合 */
+/**字符的集合
+ * [adsfsdf] 从中括号集合中任取一个字符
+ * [^adsfsdf] 排除中括号内的字符的任意一个字符
+ * 
+ * 字符边界
+ * ^a  表示以特定字符开头
+ * b$  表示以特定字符结尾
+ * 
+ * 数量词
+ * a*  表示匹配前面的字符 0次或多次
+ * a+  表示匹配前面的字符1次或多次
+ * a?  表示匹配前面的字符0次或1次
+ * a{n}  表示匹配前面的字符n次
+ * a{m,n}  表示至少匹配m次，至多匹配n次
+ * a{m, }  表示至少匹配m次
+ * x|y  表示匹配x或者y
+ */
 var reg1 = /[abc]\d/gi;
 var str1 = 'sdk1jl23jkabc1';
 str1.replace(reg1,'x');
@@ -56,5 +74,23 @@ var reg3 = /[^0-9]/gi;
 var str3 = '1a2b3c';
 str3.replace(reg3,"x");
 
+/**用小括号包裹的规则内容，称之为分组 */
+var reg4 = /(ss\d)a/gi;
+var str4 = 'ss1abcd';
+/**reg.exec(str)
+ * 如果可以匹配的，返回数组
+ * 如果不能匹配，返回null
+ *      返回的数组结构
+ *      [整个正则表达式匹配到的内容，分组匹配到的内容]
+ */
+reg4.exec(str4);
 
-/**匹配开头字符是数字  第二个字符是 a-f  第三个字符是空格 */
+var reg4 = /(ss\d)a(\dxx)/gi;
+
+
+/**str.replace()
+ *      在字符串中执行查找匹配的String方法，并且使用替换字符串替换掉匹配到的子字符串
+ */
+var reg1 = /(ab){3}/gi;
+var str1 = 'abababjdksl';
+str1.replace(reg1,'x');
--- 课程练习/LS12/02.js
+++ 课程练习/LS12/02.js
@@ -23,3 +23,46 @@ str3.replace(reg3,'x');
 var reg4 = /a+bcd/gi;
 var str4 = 'abcd abcd bcdd';
 str3.replace(reg4,'x');
+
+
+/** String.prototype
+ * 
+ *      var result = str.replace(正则表达式，替换的内容);
+ *      返回值result是被替换后的字符串
+ * 
+ *      str.match(正则表达式)
+ *      返回值，如果匹配成功返回数组，匹配失败，返回null
+ * 
+ *      var result = str.search(正则表达式);
+ *      返回值是匹配位置的起始索引值
+ * 
+ * RegExp.prototype
+ * 
+ *      reg.test(str);返回值为true和false
+ *      验证字符串是否符合正则表达式规则
+ * 
+ *      reg.exec(str)匹配返回数组，不匹配返回null
+ *      [整个正则匹配到的内容，分组匹配到的内容，index开始匹配到的索引]
+ * 
+ *           
+*/
+var reg = /\d(aa)/gi;
+var str = 'laa hello';
+console.log(str.match(reg));
+
+/**(.*)  .任意的一个字符 *表示前面的字符出现0次或多次
+ * 表示文件名字符出现0次或多次
+ * (.+)
+ * 
+ * \. 对.做转义处理，匹配.
+ * (rar|zip|7zip|tgz)任意取其中一个
+ * $以前面的分组结尾
+ */
+var reg1 = /(.*)\.(rar|zip|7zip|tgz)$/;
+
+/** ^#  表示必须以#开头
+ *  [a-fA-F0-9] 从[]集合中取任意一个字符
+ *  {6} 表示前面的字符出现6次
+ *  $以前面集合的任一字符结尾
+*/
+var reg2 = /^#[a-fA-F0-9]{6}$/;
