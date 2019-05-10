c2b1dfcdf23e5e88e90af79a2bf6859a424d0e98
zhangshuo <443494891@qq.com>
2019-05-10 09:46:29 +0800

    date

--- 课程练习/LS11/Date01.js
+++ 课程练习/LS11/Date01.js
@@ -12,3 +12,13 @@ console.log(date3);
 var millionSeconds = Date.parse('2019-01-01');
 console.log(millionSeconds);
 
+// 
+var nowDate = new Date();
+console.log(nowDate);//把当前日期和时间保存为初始值
+console.log(nowDate.getDate());//获取当前月份的日期
+console.log(nowDate.getDay());//返回Date对象的星期，返回值是 0（周日） 到 6（周六） 之间的一个整数
+console.log(nowDate.getMonth());//返回月份
+console.log(nowDate.getFullYear());//以四位数字返回年份
+
+var d1 = new Date('July 01,1999 00:01:09');
+console.log(d1.getDate());//获取创建Date对象的日
